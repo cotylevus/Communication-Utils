71073896f6280235db6b64d532994ad8f41f6ea8
BestIkun <mrteliu1107@gmail.com>
2023-06-17 17:04:52 +0800

    完善BCrypt文档说明

--- Maven-utils-package/src/main/java/B加密_BCryptUtil/BCryptUtil.java
+++ Maven-utils-package/src/main/java/B加密_BCryptUtil/BCryptUtil.java
@@ -10,12 +10,19 @@ import org.mindrot.jbcrypt.BCrypt;
 
 public class BCryptUtil {
 
-    // 加密密码
+    // 加密密码 （自动加盐 默认10）
     public static String hashPassword(String password) {
         // 生成带有盐的哈希密码，BCrypt会自动生成盐并将其附加到哈希结果中
         String hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt());
         return hashedPassword;
     }
+    // 加密密码 （手动加盐）
+    public static String hashPassword(String password , Integer salts) {
+        // 生成带有盐的哈希密码，BCrypt会自动生成盐并将其附加到哈希结果中
+        String hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt(salts));
+        return hashedPassword;
+    }
+
 
     // 验证密码
     public static boolean verifyPassword(String password, String hashedPassword) {
@@ -24,28 +31,3 @@ public class BCryptUtil {
         return passwordMatch;
     }
 }
-
-
-/*------------------------加密解密方法----------------------------*/
-
-    /*public static void main(String[] args) {
-        String password = "myPassword123";
-
-        // 加密密码
-        String hashedPassword = hashPassword(password);
-        System.out.println("原始密码: " + password);
-        System.out.println("加密后的密码: " + hashedPassword);
-
-        // 验证密码
-        boolean isPasswordCorrect = verifyPassword(password, hashedPassword);
-        System.out.println("验证密码: " + isPasswordCorrect);
-    }
-
-
-            <!--bcript加盐加密依赖-->
-            <dependency>
-            <groupId>org.mindrot</groupId>
-            <artifactId>jbcrypt</artifactId>
-            <version>0.4</version>
-            </dependency>
-            */
--- Maven-utils-package/src/main/java/B加密_BCryptUtil/Notes.md
+++ Maven-utils-package/src/main/java/B加密_BCryptUtil/Notes.md
@@ -1,10 +1,30 @@
-/*------------------------加密解密方法----------------------------*/
+###   这是一个轻型 Bcript 加密工具类，主要用于简单的密文加密
+
+
+
+/*-------------------------------加密方法-------------------------------*/
+
+​	hashPassword(String password)  默认salt加密 --> 返回值String
+
+   hashPassword(String password , Integer salt) 手动salt加密 --> 返回值String
+
+   verifyPassword(String password, String hashedPassword)  密码验证 --> 返回值Bolen
+
+
+
+/*------------------------加密解密示例----------------------------*/
 
     public static void main(String[] args) {
         String password = "myPassword123";
-    
+        Integer salt = 15 ;
         // 加密密码
+    
+        默认salt值
         String hashedPassword = hashPassword(password);
+    
+        //手动salt值
+        //String hashedPassword = hashPassword(password , salt);
+       
         System.out.println("原始密码: " + password);
         System.out.println("加密后的密码: " + hashedPassword);
     
@@ -22,4 +42,4 @@
             <version>0.4</version>
             </dependency>
 
-            
+​            
